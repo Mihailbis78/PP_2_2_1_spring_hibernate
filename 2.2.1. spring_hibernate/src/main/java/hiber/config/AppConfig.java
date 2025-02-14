59c44002b0c7edb986a0773f0db4d09823a4bac9
mihailbystrov <mihailbis78@gmail.com>
2025-02-14 13:22:15 +0300

    съехало поле в аппконфиге

--- 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
@@ -1,5 +1,6 @@
 package hiber.config;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -21,7 +22,12 @@ import java.util.Properties;
 @ComponentScan(value = "hiber")
 public class AppConfig {
 
+   private final Environment env;
+
    @Autowired
+   public AppConfig(Environment env) {
+      this.env = env;
+   }
 
    @Bean
    public DataSource getDataSource() {
@@ -43,6 +49,7 @@ public class AppConfig {
       props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
 
       factoryBean.setHibernateProperties(props);
+      factoryBean.setAnnotatedClasses(User.class, Car.class);
       return factoryBean;
    }
 
